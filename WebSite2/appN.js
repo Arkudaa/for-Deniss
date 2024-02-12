ca609b297151a21abdf25c68124f3db29798b549
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2024-02-12 17:44:50 +0200

    v01

---
+++ WebSite2/appN.js
@@ -0,0 +1,54 @@
+const loginForm=document.querySelector(".login-form");
+const signupForm=document.querySelector(".signup-form");
+const loginBtn=document.querySelector(".login button");
+const signupBtn=document.querySelector(".signup button");
+const backLayer=document.querySelector(".back-layer");
+
+signupBtn.addEventListener("click", () =>{
+  loginForm.classList.remove("active");
+  signupForm.classList.add("active");
+  backLayer.style.clipPath="inset(0 0 0 50%)"
+});
+
+loginBtn.addEventListener("click", () =>{
+  signupForm.classList.remove("active");
+  loginForm.classList.add("active");
+  backLayer.style.clipPath=""; 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const loginForm = document.querySelector(".login-form.active");
+const signupForm = document.querySelector(".signup-form");
+const loginBtn = document.querySelector(".login button");
+const signupBtn = document.querySelector(".signup button");
+const backLayer = document.querySelector(".back-layer");
+ 
+signupBtn.addEventListener("click", () => {
+  loginForm.classList.remove("active");
+  signupForm.classList.add("active");
+  backLayer.style.clipPath = "inset(0 0 0 50%)";
+});
+ 
+loginBtn.addEventListener("click", () => {
+  signupForm.classList.remove("active");
+  loginForm.classList.add("active");
+  backLayer.style.clipPath = "";
+});*/
